ebb0d4432e2f79d925eef3d2bdfe6a99d06da6e4
ethrythedev <58019814+ethrythedev@users.noreply.github.com>
2025-07-23 21:39:58 +1200

    api: fix console logic

--- api/modules/console.js
+++ api/modules/console.js
@@ -16,19 +16,19 @@ function getFormattedConsoleTime() {
 }
 
 module.exports = {
-    log: (message) => {
-        console.log(colors.blue("[LOG]"), message, colors.gray("(" + getFormattedConsoleTime() + ")"));
+    log: (...message) => {
+        console.log(colors.blue("[LOG]"), ...message, colors.gray("(" + getFormattedConsoleTime() + ")"));
     },
-    error: (message) => {
-        console.log(colors.red("[ERROR]"), message, colors.gray("(" + getFormattedConsoleTime() + ")"));
+    error: (...message) => {
+        console.log(colors.red("[ERROR]"), ...message, colors.gray("(" + getFormattedConsoleTime() + ")"));
     },
-    warn: (message) => {
-        console.log(colors.yellow("[WARN]"), message, colors.gray("(" + getFormattedConsoleTime() + ")"));
+    warn: (...message) => {
+        console.log(colors.yellow("[WARN]"), ...message, colors.gray("(" + getFormattedConsoleTime() + ")"));
     },
-    info: (message) => {
-        console.log(colors.cyan("[INFO]"), message, colors.gray("(" + getFormattedConsoleTime() + ")"));
+    info: (...message) => {
+        console.log(colors.cyan("[INFO]"), ...message, colors.gray("(" + getFormattedConsoleTime() + ")"));
     },
-    success: (message) => {
-        console.log(colors.green("[SUCCESS]"), message, colors.gray("(" + getFormattedConsoleTime() + ")"));
+    success: (...message) => {
+        console.log(colors.green("[SUCCESS]"), ...message, colors.gray("(" + getFormattedConsoleTime() + ")"));
     },
 };
