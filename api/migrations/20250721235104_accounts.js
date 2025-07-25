b0a16fd7b957d62269a87bf27eae742e48a16793
ethrythedev <58019814+ethrythedev@users.noreply.github.com>
2025-07-25 23:55:31 +1200

    api: more inputs in db before i push it to my pg instance

--- api/migrations/20250721235104_accounts.js
+++ api/migrations/20250721235104_accounts.js
@@ -7,11 +7,15 @@ exports.up = function(knex) {
         table.uuid('id').primary();
         table.string('username', 32).notNullable().unique();
 
-        table.text('password').notNullable();
-        
-        /* pbkdf2 */
+        table.text('password').notNullable(); // <-- hashed password, derived from pbkdf2 on the client-side
+
+        /* pbkdf2 - for password hashing */
         table.binary('pbkdf2_salt').notNullable();
-        table.integer('pbkdf2_iterations').notNullable()
+        table.integer('pbkdf2_iterations').notNullable();
+
+        /* enc_pbkdf2 - for encryption of openpgp key */
+        table.binary('enc_pbkdf2_salt').notNullable();
+        table.integer('enc_pbkdf2_iterations').notNullable();
        
         /* openpgp / final encryption */
         table.text('encrypted_openpgp_private_key').notNullable();
